f5d3657de7c6396a9cc147f7374f14a271f3f4ed
Samrath Singh Khanuja <63147553+NawabSahab04@users.noreply.github.com>
2024-04-20 21:40:52 +0530

    lab sheet 3

--- lab3.sql
+++ lab3.sql
@@ -1,15 +1,11 @@
 use sakila;
-select title, first_name, last_name from film inner join film_actor on film.film_id=film_actor.film_id
-inner join actor on actor.actor_id=film_actor.actor_id;
+select a.first_name, a.last_name, f.title from actor a inner join film_actor fm on a.actor_id = fm.actor_id  inner join film f on f.film_id = fm.film_id;
+select sum(amount) from payment group by customer_id;
+select rental_id from rental where rental_date not between '2005-01-01' and '2005-01-31';
+select sum(p.amount) from store s inner join staff st on s.store_id = st.store_id inner join payment p on p.staff_id = st.staff_id group by s.store_id;
 
-select amount from payment inner join customer on payment.customer_id = customer.customer_id;
-
-select distinct title from film inner join inventory on film.film_id = inventory.film_id 
-inner join rental on rental.inventory_id = inventory.inventory_id
-where month(return_date)!=1 or year(return_date)!=2005;
-
-select sum(amount) from payment inner join staff on payment.staff_id=staff.staff_id
-inner join store on store.store_id = staff.store_id; 
-
-select * from customer inner join rental on rental.customer_id = customer.customer_id
-where return_date<=curdate()-interval 30 day;
+SELECT customer.customer_id, 
+       CONCAT(customer.first_name, ' ', customer.last_name) AS customer_name
+FROM customer
+JOIN rental ON customer.customer_id = rental.customer_id
+WHERE rental.rental_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY);
